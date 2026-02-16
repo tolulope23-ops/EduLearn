7dc19038c3ded50c30ed9ec05d3ba2c2315ed82c
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2026-02-16 19:36:57 +0100

    feat: config import

--- src/common/database/connection.js
+++ src/common/database/connection.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import config from './config/config.js';
+import config from './config/config.cjs';
 
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
