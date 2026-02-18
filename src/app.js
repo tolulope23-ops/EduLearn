55a779f881c9991b4e15661d16d7f35ca95f6abf
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2026-02-19 00:07:21 +0100

    feat: routes to added to app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+
+import errorHandler from './common/middleware/errorHandler.middleware.js';
+import authRoutes from './module/Auth/routes/auth.route.js';
+
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
+
+app.use(errorHandler);
+
+app.use('/api/v1', authRoutes);
 
 app.use('/', (req, res) => {
   res.send('App is healthy');
