f4e24ed919bdb2e4e6e12527ff80293ee0e2a20f
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2026-02-18 10:40:12 +0100

    feat: email and local config

--- src/common/config/env.config.js
+++ src/common/config/env.config.js
@@ -2,3 +2,14 @@ import 'dotenv/config';  //importing .env
 
 export const PORT = process.env.PORT;
 
+
+//Email Configuration
+export const MAIL_HOST = process.env.MAIL_HOST;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+export const MAIL_PORT = Number(process.env.MAIL_PORT);
+export const MAIL_USER = process.env.MAIL_USER;
+
+
+//Local Development
+export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
